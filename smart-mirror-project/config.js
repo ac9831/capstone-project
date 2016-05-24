6b7d67e58ece4116ee0afd0978ca065016b3c7bc
Lee GunJun <ac9831@ajou.ac.kr>
2016-05-24 18:59:34 +0900

    중간 저장!

---
+++ smart-mirror-project/config.js
@@ -0,0 +1,34 @@
+var config = {
+    language : "en",
+    layout: "main",
+    greeting : ["Hi, sexy!", "Hey There!", "Looking Awesome!"], 
+    forcast : {
+        key : "e9e74061df40220aa9d3f2279ba437ef",
+        units : "auto" 
+    },
+    hue : {
+        ip : "192.168.1.99", 
+        uername : "as9234ho0dfhoq01f2as3yh4m0", 
+        group : "0" 
+    },
+    calendar: {
+      icals : ["https://calendar.google.com/calendar/ical/ac989832%40gmail.com/private-8c36e498d43c3d366b5fbb2ffa52c434/basic.ics"],
+      maxResults: 9, 
+      maxDays: 365 
+    },
+    giphy: {
+      key : "dc6zaTOxFJmzC"
+    },
+    traffic: {
+      key : "a6s5dg39j78qj38sjs91je9djadfa1e",
+      mode : "Driving",
+      origin : "350 5th Ave, New York, NY 10118",
+      destination : "1 Dr Carlton B Goodlett Pl, San Francisco, CA 94102",
+      name : "work",
+      reload_interval : 5
+    },
+    subway: {
+        key1 : "556778476561633931323865585a4268",
+        key2 : "42695068766163393737704746754d"
+    }
+};
